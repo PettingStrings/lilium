225a922ae1c7ebbbbaa5ec413d5777ffc46612d4
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2024-03-28 20:22:04 +0100

    2.0
    Added async methods, refactoring

--- src/lilium/manganelo/manganelo.py
+++ src/lilium/manganelo/manganelo.py
@@ -11,11 +11,12 @@ class ManganeloSearchIndex():
     def __init__(self, link:str) -> None:
         self.manga_list = {}
         self.link = link
+        self.current_page = 0
      
-    def GetPage(self, page_num:int)->list[ManganeloManga]:
-        manga_l = self.manga_list.get(page_num) 
-        if manga_l is not None:
-            return manga_l
+    def GetPage(self, page_num:int)->dict[ManganeloManga]:
+        dict_manga = self.manga_list.get(page_num) 
+        if dict_manga is not None:
+            return dict_manga
               
         resp = httpx.get(f"{self.link}?page={page_num}")
         if resp.status_code != 200:
@@ -25,16 +26,49 @@ class ManganeloSearchIndex():
         results = html.select(".panel-search-story .search-story-item .item-right h3 a")
         
         if len(results) == 0:
-            return []
+            return {}
         
-        manga_list = []
+        dict_manga = {}
         for element in results:
-            manga_list.append(ManganeloManga(element.get("title"), element.get("href")))
-        self.manga_list[page_num] = manga_list
+            m = ManganeloManga(element.get("title"), element.get("href"))
+            dict_manga[m.title] = m
+        self.manga_list[page_num] = dict_manga
         
-        return manga_list
+        return dict_manga
+    
+    def GetNextPage(self):
+        self.current_page += 1
+        return self.GetPage(self.current_page)
+    
+    async def GetPageAsync(self, page_num:int)->dict[ManganeloManga]:
+        dict_manga = self.manga_list.get(page_num) 
+        if dict_manga is not None:
+            return dict_manga
+              
+        resp = httpx.get(f"{self.link}?page={page_num}")
+        if resp.status_code != 200:
+            raise Exception(f"Failed to load Manga List, status code {resp.status_code} on page {page_num}")
+        
+        html = Soup(resp.content.decode(),"html.parser")
+        results = html.select(".panel-search-story .search-story-item .item-right h3 a")
+        
+        if len(results) == 0:
+            return {}
+        
+        dict_manga = {}
+        for element in results:
+            m = ManganeloManga(element.get("title"), element.get("href"))
+            dict_manga[m.title] = m
+        self.manga_list[page_num] = dict_manga
+        
+        return dict_manga
+    
+    def GetNextPageAsync(self):
+        self.current_page += 1
+        return self.GetPageAsync(self.current_page) 
+    
 
 class Manganelo():
 
-    def build_manga_searcher(query:str)->ManganeloSearchIndex:
+    def BuildMangaSearcher(query:str)->ManganeloSearchIndex:
         return ManganeloSearchIndex(f"{url}{search_url}/{query.replace(" ", "_")}")  
--- tests/testAsync.py
+++ tests/testAsync.py
@@ -1,39 +1,39 @@
 import unittest
 import shutil
 from lilium.manganelo import *
-
-class Test(unittest.TestCase):
+#TODO
+class TestAsync(unittest.IsolatedAsyncioTestCase):
 
     temp_folder = "temp"
     skip_intensive = True
 
-    def test_get_exactly_one_manga(self):
+    async def test_get_exactly_one_manga_async(self):
         manga_title = "Uratarou"
         si:ManganeloSearchIndex = Manganelo.BuildMangaSearcher(manga_title)
         
         self.assertEqual(len(si.manga_list),0) #check dict is empty
 
-        manga_l = si.GetPage(1)
+        manga_l = await si.GetPageAsync(1)
         
         self.assertEqual(len(manga_l), 1)
         self.assertEqual(len(si.manga_list), 1)
         self.assertEqual(len(si.manga_list.get(1)), 1)
         self.assertEqual(manga_l[list(manga_l.keys())[0]].title, manga_title)
     
-    def test_get_page_with_0_result(self):
+    async def test_get_page_with_0_result_async(self):
         manga_title = "Boku No Hero Academia "
-        si = Manganelo.BuildMangaSearcher(manga_title)
-        manga_l = si.GetPage(999)
+        si:ManganeloSearchIndex = Manganelo.BuildMangaSearcher(manga_title)
+        manga_l = await si.GetPageAsync(999)
         self.assertEqual(len(manga_l), 0)
         self.assertEqual(len(si.manga_list), 0)
     
     @unittest.skipIf(skip_intensive, "Time consuming test")
-    def test_download_one_chapter(self):
+    def test_download_one_chapter_async(self):
         manga = ManganeloManga("Boku No Hero Academia", "https://chapmanganelo.com/manga-jq89184")
         manga.download_chapter(1, self.temp_folder)
         
     @unittest.skipIf(skip_intensive, "Time consuming test")
-    def test_download_all_chapters(self):
+    def test_download_all_chapters_async(self):
         manga = ManganeloManga("Boku No Hero Academia", "https://chapmanganelo.com/manga-jq89184")
         manga.download_all_chapters(self.temp_folder)
     
